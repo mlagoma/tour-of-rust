f18199bd65847b2dc3dc271e00e46201185132f4
Michael Lagomarsino <mike@radom.network>
2022-05-29 18:31:42 +0100

    Ch5 - Heirarchical ownership

--- src/ownership.rs
+++ src/ownership.rs
@@ -2,16 +2,26 @@ struct Foo {
     x: i32,
 }
 
+struct Bar {
+    foo: Foo,
+}
+
 pub fn main() {
-    // We instantiate structs and bind to variables
-    // to create memory resources
-    let foo_a = Foo { x: 42 };
-    // foo is the owner
-    let foo_b = Foo { x: 13 };
+//     // We instantiate structs and bind to variables
+//     // to create memory resources
+//     let foo_a = Foo { x: 42 };
+//     // foo is the owner
+//     let foo_b = Foo { x: 13 };
 
-    println!("{}", foo_a.x);
+//     println!("{}", foo_a.x);
 
-    println!("{}", foo_b.x);
-    // foo_b is dropped here 
-    // foo_a is dropped here
+//     println!("{}", foo_b.x);
+//     // foo_b is dropped here 
+//     // foo_a is dropped here
+// }
+    // Heirarchical ownership
+    let bar = Bar { foo: Foo { x: 42 } };
+    println!("{}", bar.foo.x);
+    // bar is dropped first
+    // then bar.foo is dropped
 }
