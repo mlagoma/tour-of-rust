8e63b73ab287ade16d205ed2909abb7325686b4d
Michael Lagomarsino <mike@radom.network>
2022-05-30 23:02:21 +0100

    Ch6 - String Literals From File

--- src/text.rs
+++ src/text.rs
@@ -1,25 +1,29 @@
+use indoc::indoc;
+
 pub fn main() {
     let a: &'static str = "hi 🦀";
-    println!("{} {}", a, a.len());
+    println!("{} {}\n", a, a.len());
 
     let a: &'static str = "Ferris says:\t\"hello\"";
-    println!("{}",a);
+    println!("{}\n",a);
 
-    let haiku: &'static str = "
+    let haiku: &'static str = indoc!("
         I write, erase, rewrite
         Erase again, and then
         A poppy blooms.
-        - Katsushika Hokusai";
-    println!("{}", haiku);
-    
-    
+        - Katsushika Hokusai");
+    println!("{}\n", haiku);
+
     println!("hello \
     world"); // notice that the spacing before w is ignored
 
-    let a: &'static str = r#"
+    let a: &'static str = indoc!(r#"
         <div class="advice">
             Raw strings are useful for some situations.
         </div>
-        "#;
-    println!("{}", a);
+        "#);
+    println!("{}\n", a);
+
+    let hello_html = include_str!("hello.html");
+    println!("{}\n", hello_html);
 }
