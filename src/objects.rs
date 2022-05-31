b59b45ca329585cae5fac631dd46cc3f57a70895
Michael Lagomarsino <mike@radom.network>
2022-05-31 07:05:49 +0100

    Ch7 - Box

--- src/objects.rs
+++ src/objects.rs
@@ -29,11 +29,11 @@ impl NoiseMaker for SeaCreature {
 
 impl LoudNoiseMaker for SeaCreature {}
 
-fn static_make_noise(creature: &SeaCreature) {
+fn static_make_noise(ferris: &SeaCreature) {
     // we know the real type
-    print_type_of(&creature);
+    print_type_of(&ferris);
     print!(" goes ");
-    creature.make_noise();
+    ferris.make_noise();
 }
 
 fn dynamic_make_noise(noise_maker: &dyn NoiseMaker) {
@@ -47,34 +47,67 @@ fn print_type_of<T>(_: &T) {
     print!("{}", std::any::type_name::<T>())
 }
 
-// fn generic_make_noise<T>(creature: &T)
+// fn generic_make_noise<T>(ferris: &T)
 // where
 //     T: NoiseMaker,
 // {
 //     // we know the real type at compile-time
-//     print_type_of(&creature);
+//     print_type_of(&ferris);
 //     print!(" goes ");
-//     creature.make_noise();
+//     ferris.make_noise();
 // }
 
-fn generic_make_noise(creature: &impl NoiseMaker)
+fn generic_make_noise(ferris: &impl NoiseMaker)
 {
     // we know the real type at compile-time
-    print_type_of(&creature);
+    print_type_of(&ferris);
     print!(" goes ");
-    creature.make_noise();
+    ferris.make_noise();
+}
+
+struct Ocean {
+    animals: Vec<Box<dyn NoiseMaker>>,
 }
 
 pub fn main() -> SeaCreature {
-    let creature = SeaCreature {
+// pub fn main() -> impl NoiseMaker {
+// pub fn main() -> &'static Box<dyn NoiseMaker> {
+// pub fn main() -> &'static Box<(dyn NoiseMaker + 'static)> {
+    let ferris = SeaCreature {
         name: String::from("Ferris"),
         noise: String::from("blub"),
     };
-    println!("{} goes {}", creature.name, creature.get_sound());
-    print!("{} goes ", creature.name);
-    creature.make_alot_of_noise();
-    static_make_noise(&creature);
-    dynamic_make_noise(&creature);
-    generic_make_noise(&creature);
-    creature
+    let sarah = SeaCreature {
+        name: String::from("Sarah"),
+        noise: String::from("swish"),
+    };
+
+    println!("{} goes {}", ferris.name, ferris.get_sound());
+    print!("{} goes ", ferris.name);
+    ferris.make_alot_of_noise();
+    static_make_noise(&ferris);
+    dynamic_make_noise(&ferris);
+    generic_make_noise(&ferris);
+
+    let ocean = Ocean {
+        animals: vec![Box::new(ferris), Box::new(sarah)],
+    };
+
+    for a in ocean.animals.iter() {
+        a.make_noise();
+        // let creature = *a;
+    }
+
+    // let sarah = *&ocean.animals[1];
+    // // sarah.make_noise();
+    // sarah
+    let watisthis = &ocean.animals[1];
+    print_type_of(&watisthis);
+    println!();
+    // creature
+    // How to return the Box?
+    SeaCreature {
+        name: String::from("Mike"),
+        noise: String::from("derp"),
+    }
 }
