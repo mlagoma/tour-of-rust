d4326dc181619f9c6f0298a335b1b3741674e62d
Michael Lagomarsino <mike@radom.network>
2022-05-31 14:12:02 +0100

    Ch8 - Smart Pointers

--- src/references.rs
+++ src/references.rs
@@ -1,9 +1,22 @@
+use std::ops::Deref;
+
 struct Foo {
     value: i32
 }
 
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
+}
+
+struct TattleTell<T> {
+    value: T,
+}
+impl<T> Deref for TattleTell<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        println!("{} was used!", std::any::type_name::<T>());
+        &self.value
+    }
 }
 
 pub fn main() {
@@ -24,4 +37,14 @@ pub fn main() {
     let f = Foo { value: 42 };
     let ref_ref_ref_f = &&&f;
     println!("{}", ref_ref_ref_f.value);
+
+    let foo = TattleTell {
+        value: "secret message",
+    };
+    // dereference occurs here immediately 
+    // after foo is auto-referenced for the
+    // function `len`
+    println!("after deref: {}", foo.value);
+    println!("{}", foo.len());
+    println!("after deref: {}", foo.value);
 }
