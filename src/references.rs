a75ac2f49f89f0b4a0460a2df352105c5bb1c338
Michael Lagomarsino <mike@radom.network>
2022-05-31 14:47:11 +0100

    Ch8 - Smart Unsafe Code

--- src/references.rs
+++ src/references.rs
@@ -47,4 +47,24 @@ pub fn main() {
     println!("after deref: {}", foo.value);
     println!("{}", foo.len());
     println!("after deref: {}", foo.value);
+
+    let a: [u8; 4] = [86, 14, 73, 64];
+    // this is a raw pointer. Getting the memory address
+    // of something as a number is totally safe
+    println!("{}", a[0]);
+    let pointer_a = &a as *const u8 as usize;
+    println!("Data memory location: {}", pointer_a);
+    // Turning our number into a raw pointer to a f32 is
+    // also safe to do.
+    let pointer_b = pointer_a as *const f32;
+    let pointer_c = pointer_b as *const f32;
+    println!("Data memory location: {}", pointer_b as usize);
+    let b = unsafe {
+        // This is unsafe because we are telling the compiler
+        // to assume our pointer is a valid f32 and
+        // dereference it's value into the variable b.
+        // Rust has no way to verify this assumption is true.
+        *pointer_c
+    };
+    println!("I swear this is a pie! {}", b);
 }
