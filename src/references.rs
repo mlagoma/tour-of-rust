4fa0411a4bc21fafcaba1f14e4e41090f0e9f02e
Michael Lagomarsino <mike@radom.network>
2022-05-31 15:05:51 +0100

    Ch8 - Unsafe dereference of smart pointers, structs, memory allocations

--- src/references.rs
+++ src/references.rs
@@ -1,4 +1,5 @@
 use std::ops::Deref;
+use std::alloc::{alloc, Layout};
 
 struct Foo {
     value: i32
@@ -16,6 +17,38 @@ impl<T> Deref for TattleTell<T> {
     fn deref(&self) -> &T {
         println!("{} was used!", std::any::type_name::<T>());
         &self.value
+    }
+}
+
+struct Pie {
+    secret_recipe: usize,
+}
+impl Pie {
+    fn new() -> Self {
+        // let's ask for 4 bytes
+        let layout = Layout::from_size_align(4, 1).unwrap();
+
+        unsafe {
+            // allocate and save the memory location as a number
+            let ptr = alloc(layout) as *mut u8;
+            // use pointer math and write a few 
+            // u8 values to memory
+            ptr.write(86);
+            ptr.add(1).write(14);
+            ptr.add(2).write(73);
+            ptr.add(3).write(64);
+
+            Pie { secret_recipe: ptr as usize }
+        }
+    }
+}
+impl Deref for Pie {
+    type Target = f32;
+    fn deref(&self) -> &f32 {
+        // interpret secret_recipe pointer as a f32 raw pointer
+        let pointer = self.secret_recipe as *const f32;
+        // dereference it into a return value &f32
+        unsafe { &*pointer }
     }
 }
 
@@ -67,4 +100,10 @@ pub fn main() {
         *pointer_c
     };
     println!("I swear this is a pie! {}", b);
+
+    let p = Pie::new();
+    println!("{}", p.secret_recipe);
+    // "make a pie" by dereferencing our 
+    // Pie struct smart pointer
+    println!("{:?}", *p);
 }
